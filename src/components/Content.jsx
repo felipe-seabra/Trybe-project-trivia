a1a0306bbfef024913cecf9a2714da2c7a8044b6
Thais Rosa <tsrosa18@gmail.com>
2022-10-11 20:44:52 -0300

    requisito 16

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,6 +4,7 @@ import Login from '../pages/Login';
 import Game from '../pages/Game';
 import Settings from '../pages/Settings';
 import Feedback from '../pages/Feedback';
+import Ranking from '../pages/Ranking';
 
 export default class Content extends React.Component {
   render() {
@@ -13,6 +14,7 @@ export default class Content extends React.Component {
         <Route path="/game" component={ Game } />
         <Route path="/settings" component={ Settings } />
         <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     );
   }
---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Ranking extends Component {
+  render() {
+    return (
+      <div data-testid="ranking-title">Ranking</div>
+    );
+  }
+}
+export default Ranking;
