b0944870c11cc54c2630f8a60fb34c623eb7ecca
Thais Rosa <tsrosa18@gmail.com>
2022-10-11 20:53:34 -0300

    requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <div data-testid="ranking-title">
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Home
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
 export default Ranking;
